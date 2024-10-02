659ddd9c0dad147613a62cd96233cad2111876f5
PratapRash <prathap0688@gmail.com>
2024-10-03 05:13:51 +0530

    commit 1

---
+++ testjenkins/src/main/java/testjenkins/demo.java
@@ -0,0 +1,19 @@
+package testjenkins;
+
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test
+	public void test() {
+		
+		System.out.println("testing jsldkjfl lkjdlkjfskdj");
+	}
+	
+	@Test
+	public void test1() {
+		
+		System.out.println("dsfadfajdfj  asdfdfsafdl sfadf");
+	}
+
+}
